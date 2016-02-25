6254a7debabc84dc2d5fa32dcf6dd17250c3342c
l3v3l <devl3v3l@gmail.com>
2016-02-25 08:55:26 +0000

    removing extra empty lines

--- src/main.py
+++ src/main.py
@@ -6,14 +6,12 @@ selector = "text"
 xselector = "ex"
 replacer = "cena"
 
-
 def find(selector, body):
     """find all occurences of selection"""
     findArray = []
     for m in re.finditer(selector, body):
          findArray.append([m.start(), m.end()])
     return findArray
-
 
 def replace(replacer,iteratorArray,body):
     """replace all selection with replacer"""
@@ -24,13 +22,11 @@ def replace(replacer,iteratorArray,body):
         body = body[:i[0]] + replacer + body[i[0]:]
     return body
 
-
 def selectionAtStart(iteratorArray):
     """shrink selection to start of selection"""
     for i in iteratorArray:
         i[1] = i[0]
     return iteratorArray
-
 
 def selectionAtEnd(iteratorArray):
     """shrink selection to end of selection"""
@@ -44,7 +40,6 @@ def moveSelection(amount,iteratorArray):
         i[1]= i[1] + amount
     return iteratorArray
 
-
 def expandSelection(amount,iteratorArray):
     """exapanding or contracting selections"""
     for i in iteratorArray:
@@ -53,7 +48,6 @@ def expandSelection(amount,iteratorArray):
         if i[1] < i[0]:
             i[1] = i[0]
     return iteratorArray
-
 
 def fixSelections(body, iteratorArray ):
     """fixing selections"""
@@ -69,7 +63,6 @@ def fixSelections(body, iteratorArray ):
             i[1] = len(body)
     #TODO check if overlapping
     return iteratorArray
-
 
 def findInsideSelection(iteratorArray, body, selector):
     """finding inside a selection"""
