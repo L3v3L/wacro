64cb5436652d8d10ac9c9f5b943d737f910b06c3
L3v3L <devl3v3l@gmail.com>
2016-02-29 11:06:05 +0000

    added wacro terminal, and a print of current state of input text with selections

--- src/main.py
+++ src/main.py
@@ -2,12 +2,35 @@
 import sys, argparse, logging, re
 from StringOperations import *
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 def printSelections(body,iteratorArray):
     """prints the current contents of iteratorArray"""
     logging.debug("start printSelections")
     for i in iteratorArray:
         print body[i[0]:i[1]]
-        
+
+def printInputTextWithSelections(body, iteratorArray):
+    """prints out the inputted text with the current selections"""
+    logging.debug("start printInputTextWithSelections")
+    last = 0
+    output=""
+    for i in iteratorArray:
+        output+= body[last:i[0]]
+        output+= (bcolors.OKGREEN + body[i[0]:i[1]] + bcolors.ENDC)
+        last = i[1]
+
+    output+=  body[last:]
+    print output
+   
 def tests():
     """running a few tests"""
     logging.debug("start tests")
@@ -39,7 +62,12 @@ def runCommands(commands,inputText):
     #print "Text to be Wacrod: " + inputText
 
     #remove casing
-    commands = commands[1:-1]
+    if commands[0] == "[":
+        if commands[-1]== "]":
+            commands = commands[1:-1]
+        else:
+            print "Error in capsulating"
+            return inputText
 
     #split commands
     commandArray = commands.split(",")
@@ -54,9 +82,9 @@ def runCommands(commands,inputText):
         if fun == "f" or fun=="find":
             newiteratorArray = find(para,inputText)
         if fun == "r" or fun == "replace":
-            print replace(para,newiteratorArray,inputText)
+            inputText = replace(para,newiteratorArray,inputText)
         if fun == "ss" or fun == "start":
-            newiteratorArray = selectionAtStart(newiteratorArray)
+           newiteratorArray = selectionAtStart(newiteratorArray)
         if fun == "se" or fun == "end":
             newiteratorArray = selectionAtEnd(newiteratorArray)
         if fun == "ms" or fun == "move":
@@ -71,6 +99,10 @@ def runCommands(commands,inputText):
             newiteratorArray = selectAllCurrentLine(inputText, newiteratorArray)
         if fun == "ps" or fun == "print_selection":
             printSelections(inputText, newiteratorArray)
+        if fun == "p" or fun == "print ":
+            printInputTextWithSelections(inputText, newiteratorArray)
+    return inputText
+
 
 def loadInputFile(path):
     """load input file"""
@@ -112,6 +144,15 @@ def main(args, loglevel):
     else:
         print "missing input text and/or commands"
 
+    if args.terminal:
+        flag = 0
+        while(flag == 0):
+            inp = raw_input(bcolors.OKBLUE+"\nwacro > "+bcolors.ENDC)
+            if inp == "exit":
+                flag = 1
+            else:
+                runCommands(inp,inputText)
+            
 if (__name__ == '__main__'):
     #if len(sys.argv) < 2:
     #    sys.argv.append("-v")
@@ -163,12 +204,19 @@ if (__name__ == '__main__'):
         action="store_true")
 
     parser.add_argument(
-        "-t",
         "--test",
         help ="runs tests",
         default=False,
         action="store_true")
-    
+
+    parser.add_argument(
+        "-t",
+        "--terminal",
+        help ="starts wacro terminal",
+        default=False,
+        action="store_true")
+
+
     args = parser.parse_args()
   
     # Setup logging
